6b75fa2470203beff5d432a1ca139fc52b32e101
Chemobo <jkchemobo@gmail.com>
2022-10-06 22:43:38 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    #post validations
+    validates :title, presence: true
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validates :content, length: {minimum: 100}
 end
